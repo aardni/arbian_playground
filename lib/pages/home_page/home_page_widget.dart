5e5b689ec0913a81bf1d6861b7d5a159165a40f8
arbian <arbianindra@gmail.com>
2023-05-25 22:53:15 +0700

    🐞 fix: testing

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -3,8 +3,6 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/form_field_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'home_page_model.dart';
 export 'home_page_model.dart';
 
@@ -46,10 +44,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Page Title',
+            'Page',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Outfit',
-                  color: Colors.white,
+                  color: Colors.grey,
                   fontSize: 22.0,
                 ),
           ),
@@ -86,6 +84,12 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 margin: EdgeInsetsDirectional.fromSTEB(16.0, 4.0, 16.0, 4.0),
                 hidesUnderline: true,
                 isSearchable: false,
+              ),
+              Text(
+                "OII WORKS GAK",
+                style: TextStyle(
+                  fontSize: 15.0,
+                ),
               ),
             ],
           ),
